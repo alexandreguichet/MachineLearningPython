74698c7cd3c9b3a99fedafee4b3de4a46366a0e7
Salade_de_Poney <alexandre.g007@gmail.com>
2019-02-08 10:24:21 -0800

    Alex : multiple regression

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/data_preprocessing.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/data_preprocessing.py
@@ -24,12 +24,12 @@ class PreprocessingData:
         self.dependantVariables = self.dataset.iloc[:, -1:].values # The dependant variables of the dataset imported [Purchased]
 
     #Splitting the dataset into the Training set and Test set
-    def seperate_training_test_set(self, test_size = 0.2, random_state = 0):
+    def seperate_training_test_set(self, test_size_training = 0.2, random_state_training = 0):
         from sklearn.model_selection import train_test_split
         self.independantVariables_TrainingSet, self.independantVariables_TestSet, self.dependantVariables_TrainingSet, self.dependantVariables_TestSet = train_test_split(self.independantVariables, 
                                                                                                                                               self.dependantVariables, 
-                                                                                                                                              test_size = 1/3, 
-                                                                                                                                              random_state = 0)
+                                                                                                                                              test_size = test_size_training, 
+                                                                                                                                              random_state = random_state_training)
 
     #Taking care of missing data
     def adjust_missing_data(self):
@@ -40,20 +40,20 @@ class PreprocessingData:
 
 
     #Encoding categorical data
-    def encode_categorical_data(self):       
+    def encode_categorical_data(self, has_dependant_categorical = False):       
         from sklearn.preprocessing import LabelEncoder, OneHotEncoder
         labelEncoderIndependantVariables = LabelEncoder()
         for x in range (0, self.dataset.shape[1]):
-            print("I enter the for loop")
             if type(self.dataset.iloc[1, x]) == str:
-                print("I found a category in my column ! (str)")
                 self.independantVariables[:, x] = labelEncoderIndependantVariables.fit_transform(self.independantVariables[:, x])
                 oneHotEncoderIndependantVariables = OneHotEncoder(categorical_features= [x])
                 self.independantVariables = oneHotEncoderIndependantVariables.fit_transform(self.independantVariables).toarray()
+        self.avoid_dummy_variable_trap()
 
     #TODO: change the line bellow if we need more than two variables output (Else than a yes/no answer)
-        labelEncoderDependantVariables = LabelEncoder()
-        self.dependantVariables = labelEncoderDependantVariables.fit_transform(self.dependantVariables) #No need for OneHotEncoder for this one because there is no more than 2 outputs: (Yes/No)
+        if has_dependant_categorical:
+            labelEncoderDependantVariables = LabelEncoder()
+            self.dependantVariables = labelEncoderDependantVariables.fit_transform(self.dependantVariables) #No need for OneHotEncoder for this one because there is no more than 2 outputs: (Yes/No)
 
     #Feature scaling
     def feature_scaling(self):
@@ -62,8 +62,13 @@ class PreprocessingData:
         self.independantVariables_TrainingSet = scale_independantVariables.fit_transform(self.independantVariables_TrainingSet)
         self.independantVariables_TestSet = scale_independantVariables.transform(self.lindependantVariables_TestSet)
         
+    #Avoiding the Dummy Variable Trap
+    def avoid_dummy_variable_trap(self):
+        self.independantVariables = self.independantVariables[:, 1:] 
+        
+    #Getters for variables
     def get_imported_variables(self):
-        return self.dataset, self.independantVariables, self.independantVariables
+        return self.dataset, self.independantVariables, self.dependantVariables
     
     def get_trainingSet_variables(self):
         return self.independantVariables_TrainingSet, self.independantVariables_TestSet, self.dependantVariables_TrainingSet, self.dependantVariables_TestSet
--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -9,13 +9,25 @@ Multible Linear Regression
 #Import libraries
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 from data_preprocessing import PreprocessingData
 
+#Preprocess using Preprocessing Custom Class
 preprocessing = PreprocessingData()
 preprocessing.import_dataset("50_Startups.csv")
 preprocessing.adjust_missing_data()
 preprocessing.encode_categorical_data()
-preprocessing.seperate_training_test_set(test_size = 1/3, random_state = 0)
+preprocessing.seperate_training_test_set(test_size_training = 0.2, random_state_training = 0)
 dataset, independantVariables, dependantVariables = preprocessing.get_imported_variables()
 independantVariables_TrainingSet, independantVariables_TestSet, dependantVariables_TrainingSet, dependantVariables_TestSet = preprocessing.get_trainingSet_variables()
+
+#Fitting Multiple LinearRegression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(independantVariables_TrainingSet, dependantVariables_TrainingSet)
+
+#Predicting the Test set results
+prediction_vector = regressor.predict(independantVariables_TestSet)
+
+#Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+independantVariables = np.append(arr = np.ones(50,1).astype(int), values = independantVariables, axis = 1)
